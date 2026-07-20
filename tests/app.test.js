4ea7a2d99e26e6bb78ba077d0734792c53f24ba4
Majedul Hasan <hasanmajedul@gmail.com>
2026-07-20 13:27:41 -0700

    Implement code changes to enhance functionality and improve performance

---
+++ tests/app.test.js
@@ -0,0 +1,16 @@
+const request = require('supertest');
+const app = require('../app');
+
+describe('GET /', () => {
+  it('respond with text "Hello, World!"', async () => {
+
+    const chai= await import('chai');
+    const expect  = chai.expect;
+    await request(app)
+    .get('/')
+    .expect(200)
+    .then((res) => {
+      expect(res.text).to.equal('Hello, World!');
+    });
+  });
+});
